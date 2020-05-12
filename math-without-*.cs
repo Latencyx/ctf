4f6adfe1b0ecc2295efc4fc05d0aa6c4339dd3e6
martin3lli <44301274+Latencyx@users.noreply.github.com>
2020-05-12 11:31:01 -0300

    Create math-without-*.cs

---
+++ math-without-*.cs
@@ -0,0 +1,15 @@
+        static long mutilplicador(long n)
+        {
+            long prod = (n << 4);
+            prod = prod - n;
+
+            return prod;
+        }
+
+        static void Main(string[] args)
+        {
+            long n = 15;
+            Console.Write(mutilplicador(n));
+            Console.ReadLine();
+        }
+    }
